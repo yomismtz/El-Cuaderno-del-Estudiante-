d6f3dfe3565f1a4690b6b7373c955d360f84d5b1
yomismtz <yomismtz@gmail.com>
2026-09-09 17:14:08 -0600

    Add app/src/main/java/com/cuadernoestudiante/app/data/repository/StudentRepository.kt

---
+++ app/src/main/java/com/cuadernoestudiante/app/data/repository/StudentRepository.kt
@@ -0,0 +1,10 @@
+package com.cuadernoestudiante.app.data.repository
+
+import com.cuadernoestudiante.app.core.model.StudentDataSnapshot
+import kotlinx.coroutines.flow.StateFlow
+
+interface StudentRepository {
+    val snapshot: StateFlow<StudentDataSnapshot>
+
+    fun resetDemoData()
+}
