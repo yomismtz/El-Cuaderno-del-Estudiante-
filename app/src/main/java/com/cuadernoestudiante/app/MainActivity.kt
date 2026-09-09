2d4cee2f973812c8267159f46fb97dfffa60b487
yomismtz <yomismtz@gmail.com>
2026-09-09 17:13:55 -0600

    Add app/src/main/java/com/cuadernoestudiante/app/MainActivity.kt

---
+++ app/src/main/java/com/cuadernoestudiante/app/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.cuadernoestudiante.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.cuadernoestudiante.app.data.demo.DemoStudentRepository
+import com.cuadernoestudiante.app.ui.theme.StudentTheme
+
+class MainActivity : ComponentActivity() {
+    private val repository by lazy { DemoStudentRepository() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            val studentViewModel: StudentViewModel = viewModel(
+                factory = StudentViewModel.Factory(repository)
+            )
+            StudentTheme {
+                StudentApp(viewModel = studentViewModel)
+            }
+        }
+    }
+}
