8921b0cf7a0f8a4908847cd7a1115b5109597462
yomismtz <yomismtz@gmail.com>
2026-09-09 17:14:43 -0600

    Add app/src/main/java/com/cuadernoestudiante/app/ui/theme/Color.kt

---
+++ app/src/main/java/com/cuadernoestudiante/app/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.cuadernoestudiante.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val StudentPurple = Color(0xFF65558F)
+val StudentPurpleDark = Color(0xFF4F378B)
+val StudentMint = Color(0xFFCDEDE3)
+val StudentTurquoise = Color(0xFF006A6A)
+val StudentSurface = Color(0xFFFFFBFE)
